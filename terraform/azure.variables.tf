cacd02aa2335b26485ec59bf3ca48cf1d4e6487c
Onno <onno@onknows.com>
2018-07-10 18:18:15 +0200

    DIC-57 public fixed ip

--- terraform/azure.variables.tf
+++ terraform/azure.variables.tf
@@ -4,8 +4,8 @@ variable "azure_location" {}
 variable "azure_subscription_id" {}
 variable "azure_tenant_id" {}
 variable "openshift-public-ip" {
-  default = "/subscriptions/9e54bcbd-ad57-485e-a4c4-5fb4e661fb9f/resourceGroups/cloud-shell-storage-westeurope/providers/Microsoft.Network/publicIPAddresses/dop-public-ip"
+  default = "/subscriptions/7f758ce0-5f13-401a-b1da-973585722bb6/resourceGroups/c2-resources/providers/Microsoft.Network/publicIPAddresses/zgw-public-ip"
 }
 variable "bastion-public-ip" {
-  default = "/subscriptions/9e54bcbd-ad57-485e-a4c4-5fb4e661fb9f/resourceGroups/cloud-shell-storage-westeurope/providers/Microsoft.Network/publicIPAddresses/dop-public-ip-2"
+  default = "/subscriptions/7f758ce0-5f13-401a-b1da-973585722bb6/resourceGroups/c2-resources/providers/Microsoft.Network/publicIPAddresses/zgw-public-ip-bastion"
 }
